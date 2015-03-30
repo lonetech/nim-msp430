694dbb84989c0ec2138c739bf0efc178ebc1d55a
Yann Vernier <yann.vernier@orsoc.se>
2015-03-30 10:22:19 +0200

    Support files for converting MSP430 headers.

---
+++ iomacros.nim
@@ -0,0 +1,28 @@
+import macros
+
+macro sfrb*(nam: expr, loc: int16): stmt {.immediate.} =
+  result = newNimNode(nnkStmtList)
+  nam.expectKind(nnkIdent)
+  let name = $nam.ident
+  result.add(parseStmt("template "&name&"*: expr = cast[ptr uint8]("&loc.repr&")[]"))
+
+macro sfrw*(nam: expr, loc: int16): stmt {.immediate.} =
+  result = newNimNode(nnkStmtList)
+  nam.expectKind(nnkIdent)
+  let name = $nam.ident
+  #let decl = 
+  #echo decl
+  result.add(parseStmt("var "&name&"* {.volatile, extern:\"__"&name&"\".}: uint16"))
+
+# Note: may have to be redefined on 20-bit msp430s
+macro sfra*(nam: expr, loc: int16): stmt {.immediate.} =
+  result = newNimNode(nnkStmtList)
+  nam.expectKind(nnkIdent)
+  let name = $nam.ident
+  result.add(parseStmt("template "&name&"*: expr = cast[ptr void]("&loc.repr&")[]"))
+
+## TODO: patch in volatile in appropriate places
+## TODO: make const registers const
+template const_sfrb*(nam,loc) = sfrb(nam,loc)
+template const_sfrw*(nam,loc) = sfrw(nam,loc)
+template const_sfra*(nam,loc) = sfra(nam,loc)
